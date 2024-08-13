bf19fb6f777eb2fc99f25499e44f9268bc6e8128
iamdramaticleo <iamdramaticleo@gmail.com>
2024-08-13 22:26:59 +0300

    Remove redundant GLFW documentation files
    Deleted obsolete build, CMake, and compatibility documentation from GLFW docs. These files are no longer necessary for the current project setup and maintaining them would lead to outdated and redundant information.

---
+++ code/platform/public.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string>
+
+#include <GLFW/glfw3.h>
---
+++ code/platform/window.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace engine
+{
+    class Window
+    {
+    public:
+        void create(const std::string& title, uint32_t width, uint32_t height);
+        void update() const;
+        void destroy() const;
+
+        bool is_closed() const;
+
+    private:
+        GLFWwindow* _handle;
+    };
+}
