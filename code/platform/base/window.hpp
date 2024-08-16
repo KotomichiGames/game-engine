83c50924c6aae32a1227c0a8a01c5fe9a4929553
iamdramaticleo <iamdramaticleo@gmail.com>
2024-08-16 20:29:00 +0300

    Refactor window state management in GLFW platform
    Removed `glfwWindowShouldClose` and related functions, transitioning to a new `window_state` enum for managing window states. Updated relevant header and source files to reflect these changes and added new CMakeLists for core and graphics.

--- code/platform/base/window.hpp
+++ code/platform/base/window.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "window_state.hpp"
+
 namespace engine::base
 {
     class Window
@@ -9,6 +11,19 @@ namespace engine::base
         virtual void update()  const = 0;
         virtual void destroy() const = 0;
 
-        [[nodiscard]] virtual bool is_closed() const = 0;
+        virtual ~Window() = default;
+
+        void state(const window_state state)
+        {
+            _state = state;
+        }
+
+        [[nodiscard]] window_state state() const
+        {
+            return _state;
+        }
+
+    private:
+        window_state _state;
     };
 }
