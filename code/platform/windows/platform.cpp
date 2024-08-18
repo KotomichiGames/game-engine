5b48ea92221688ac8697621b8b14910794694175
iamdramaticleo <iamdramaticleo@gmail.com>
2024-08-19 00:16:33 +0300

    Add window event processing in Win32 platform implementation
    Introduced `process_events` method to handle window messages, including close events, using a switch-case structure. Updated `Window::create` to register a window class and associate it with this new method. Improved `Window::destroy` by properly destroying the window and unregistering its class.

--- code/platform/windows/platform.cpp
+++ code/platform/windows/platform.cpp
@@ -1,8 +1,37 @@
 #include "platform.hpp"
+#include "window_instance.hpp"
 
 namespace engine::win32
 {
+    LRESULT Platform::process_events(const HWND hwnd, const UINT msg, const WPARAM wparam, const LPARAM lparam)
+    {
+        switch (msg)
+        {
+            case WM_CLOSE:
+            {
+                WindowInstance::instance().close();
+                return 0;
+            }
+        }
+
+        return DefWindowProc(hwnd, msg, wparam, lparam);
+    }
+
     void Platform::update() const
     {
+        MSG msg;
+
+        while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+            {
+                WindowInstance::instance().close();
+            }
+            else
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
     }
 }
--- code/platform/windows/window.cpp
+++ code/platform/windows/window.cpp
@@ -1,21 +1,38 @@
 #include "window.hpp"
+#include "platform.hpp"
 
 namespace engine::win32
 {
     void Window::create(const std::string& title)
     {
+        const HINSTANCE   instance = GetModuleHandle(nullptr);
+        const WNDCLASSEX  window_class
+        {
+            .cbSize        = sizeof(WNDCLASSEX),
+            .style         = CS_HREDRAW | CS_VREDRAW | CS_OWNDC,
+            .lpfnWndProc   = Platform::process_events,
+            .hInstance     = instance,
+            .lpszClassName = title.c_str()
+        };
+
+        constexpr UINT extra = WS_EX_APPWINDOW;
+        constexpr UINT style = WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_OVERLAPPEDWINDOW;
+
+        _class  = RegisterClassEx(&window_class);
+        _handle = CreateWindowEx(extra, MAKEINTATOM(_class), title.c_str(), style, CW_USEDEFAULT, CW_USEDEFAULT,
+                                                              _size.width, _size.height, nullptr, nullptr, instance, nullptr);
+        ShowWindow(_handle, SW_SHOW);
     }
 
     void Window::destroy() const
     {
-    }
+        DestroyWindow(_handle);
 
-    void Window::update() const
-    {
+        UnregisterClass(MAKEINTATOM(_class), GetModuleHandle(nullptr));
     }
 
     std::any Window::handle() const
     {
-        return { };
+        return _handle;
     }
 }
