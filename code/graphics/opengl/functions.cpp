02c807306e8f2fc91f0aa570e883bb9979ffc1ad
iamdramaticleo <iamdramaticleo@gmail.com>
2024-08-22 22:14:28 +0300

    Implement VertexArray class and refactor OpenGL functions
    Refactor OpenGL functions from GLAD to a new VertexArray class for create, bind, and delete operations. Updated related headers and source files, and modified CMakeLists.txt to include the new vertex_array.cpp file.

--- code/graphics/opengl/functions.cpp
+++ code/graphics/opengl/functions.cpp
@@ -11,6 +11,10 @@ namespace engine::gl
         glDrawArrays   = reinterpret_cast<PFNGLDRAWARRAYSPROC>(GetProcAddress(instance, "glDrawArrays"));
         glDrawElements = reinterpret_cast<PFNGLDRAWELEMENTSPROC>(GetProcAddress(instance, "glDrawElements"));
 
+        glCreateVertexArrays = reinterpret_cast<PFNGLCREATEVERTEXARRAYSPROC>(wglGetProcAddress("glCreateVertexArrays"));
+        glBindVertexArray    = reinterpret_cast<PFNGLBINDVERTEXARRAYPROC>(wglGetProcAddress("glBindVertexArray"));
+        glDeleteVertexArrays = reinterpret_cast<PFNGLDELETEVERTEXARRAYSPROC>(wglGetProcAddress("glDeleteVertexArrays"));
+
         FreeLibrary(instance);
     }
 
