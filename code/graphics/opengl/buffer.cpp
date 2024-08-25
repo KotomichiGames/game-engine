6908a5a01b97b87588fbd9e25bbcb00c5cde5e3a
iamdramaticleo <iamdramaticleo@gmail.com>
2024-08-25 22:21:59 +0300

    Add Buffer management methods to OpenGL buffer class
    Introduced create, destroy, bind, and data methods to the Buffer class in buffer.cpp. Updated the Buffer class to inherit from Object and included necessary header files.

--- code/graphics/opengl/buffer.cpp
+++ code/graphics/opengl/buffer.cpp
@@ -1,5 +1,25 @@
 #include "buffer.hpp"
+#include "functions.hpp"
 
 namespace engine::gl
 {
+    void Buffer::create()
+    {
+        glCreateBuffers(1, &_handle);
+    }
+
+    void Buffer::destroy()
+    {
+        glDeleteBuffers(1, &_handle);
+    }
+
+    void Buffer::bind(const uint32_t target) const
+    {
+        glBindBuffer(target, _handle);
+    }
+
+    void Buffer::data(const base::buffer_data& buffer, const uint32_t usage) const
+    {
+        glNamedBufferData(_handle, buffer.size(), buffer.data(), usage);
+    }
 }
--- code/graphics/opengl/buffer.hpp
+++ code/graphics/opengl/buffer.hpp
@@ -1,10 +1,18 @@
 #pragma once
 
+#include "object.hpp"
+
 #include "base/buffer_data.hpp"
 
 namespace engine::gl
 {
-    class Buffer
+    class Buffer final : public Object
     {
+    public:
+        void create()  override;
+        void destroy() override;
+
+        void bind(uint32_t target)                                 const;
+        void data(const base::buffer_data& buffer, uint32_t usage) const;
     };
 }
