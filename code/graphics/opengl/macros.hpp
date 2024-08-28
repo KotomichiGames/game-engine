6227d69879d935f980a7015f9677d7d56ffe8be4
iamdramaticleo <iamdramaticleo@gmail.com>
2024-08-28 16:14:49 +0300

    Rename shader constants for clarity
    Updated `vertex_shader` to `vertex_stage` and `fragment_shader` to `fragment_stage` to better reflect their purpose within the OpenGL code. This change improves code readability and consistency in naming conventions.

--- code/graphics/opengl/macros.hpp
+++ code/graphics/opengl/macros.hpp
@@ -4,8 +4,8 @@ namespace engine::gl
 {
     constexpr uint32_t color_buffer_bit = 0x00004000;
 
-    constexpr uint32_t vertex_shader    = 0x8B31;
-    constexpr uint32_t fragment_shader  = 0x8B30;
+    constexpr uint32_t vertex_stage     = 0x8B31;
+    constexpr uint32_t fragment_stage   = 0x8B30;
 
     constexpr uint32_t static_draw = 0x88E4;
 
