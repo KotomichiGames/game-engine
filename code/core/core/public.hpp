3e43dc757eeb1fb659a52bd356961bd7fe2c64e9
iamdramaticleo <iamdramaticleo@gmail.com>
2024-08-31 12:12:18 +0300

    Remove private headers and adjust CMakeLists.
    Deleted `private.hpp` in core and graphics, added necessary includes to `public.hpp`. Updated CMakeLists to remove references to the removed private headers and added a new `camera.hpp` file under `data`.

--- code/core/core/public.hpp
+++ code/core/core/public.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <fstream>
 #include <vector>
 #include <chrono>
+
+#include <cassert>
 
 #include <any>
 #include <ios>
---
+++ code/core/data/camera.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace engine::data
+{
+    struct camera
+    {
+    };
+}
